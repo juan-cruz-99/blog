0e2714fc510651ad2de8d1b9553bde8e5d3b4216
Juan Cruz Ambrosino <juancruzambrosino@gamil.com>
2020-10-01 12:02:48 -0300

    Crear tabla visited_posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
     {
         return  $this->hasMany(Post::class);
     }
+
+    public function visitedPosts()
+    {
+        return $this->belongsToMany(Post::class, 'visited_posts');
+    }
 }
