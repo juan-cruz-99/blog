694f1b10c356cfc6b8dd4b2dc4f2c9f15bf87630
Juan Cruz Ambrosino <juancruzambrosino@gamil.com>
2020-10-02 10:10:28 -0300

    agegar link a post en el listado

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -3,7 +3,7 @@
     <div class="card-body">
     <h2 class="card-title">{{ $post->title }}</h2>
     <p class="card-text">{{ $post->content }}</p>
-    <a href="#" class="btn btn-primary">Read More &rarr;</a>
+    <a href="{{ route('posts.show',['post'=>$post]) }}" class="btn btn-primary">Read More &rarr;</a>
     </div>
     <div class="card-footer text-muted">
     {{ $post->created_at->diffForHumans() }}
